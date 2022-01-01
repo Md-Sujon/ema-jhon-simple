6635f64e8bea1362c9c94eb7f7d338931221e0e5
Md-Sujon <mdsujon829562@gmail.com>
2022-01-01 23:18:35 +0600

    sing out

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,32 +1,74 @@
-import React from 'react';
-import { initializeApp } from 'firebase/app';
-import firebaseConfig from '../firebase.config';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import './Login.css';
+import React, { useState } from "react";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "../firebase.config";
+import { getAuth, signInWithPopup, GoogleAuthProvider,getRedirectResult } from "firebase/auth";
+import "./Login.css";
 
-
-
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const Login = () => {
+  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({
+    isSingIn: false,
+    name: "",
+    photo: "",
+    email: "",
+  });
 
-    const provider = new GoogleAuthProvider();
-    
-     const handleSingin = () => {
-        const auth = getAuth();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    const {displayName,email,photoURL} = result.user;
+  const handleSingIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        const singIn = {
+          isSingIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(singIn);
+        console.log(displayName, email, photoURL);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-      console.log(displayName,email,photoURL);
-  })
-   }
-    return (
-        <div className="container login m-5">
-            <h3>This is Login</h3>
-            <button onClick={handleSingin}>Sing in</button>
+  const handleSingOut = () => {
+    const auth = getAuth();
+    getRedirectResult(auth)
+    .then(response => {
+      const singOut = {
+        isSingIn: false,
+        name: '',
+        email: '',
+        phone: ''
+      }
+      setUser(singOut);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+  return (
+    <div className="container login m-5">
+      <h3>This is Login</h3>
+
+      {user.isSingIn ? (
+        <button onClick={handleSingOut}>Sing Out</button>
+      ) : (
+        <button onClick={handleSingIn}>Sing In</button>
+      )}
+
+      {user.isSingIn && (
+        <div>
+          <p>WelCome, {user.name}</p>
+          <p>Email: {user.email}</p>
+          <img src={user.photo} alt="" />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Login;
